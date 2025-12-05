c2bf068967a32dc2d304ccd33011949ccbf107fb
Abdurrahmankhan11 <abdurrahmankhan1429@gmail.com>
2025-12-05 17:18:51 +0530

    Changes

--- src/components/Landing_Page/Footer.jsx
+++ src/components/Landing_Page/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className='footerBg'>
         <h2 className="dashboardFooterHeading">Address</h2>
-        <h6><i className="fa-solid fa-location-dot text-warning"></i> <a className="a" href="">Moti Masjid</a>: <span >Park, Behind Water Tank, Motia, Near Swarswati Prakashan, Sultania Rd, Madhya Pradesh 462001
+        <h6><i className="fa-solid fa-location-dot text-warning"></i> Moti Masjid: <span >Park, Behind Water Tank, Motia, Near Swarswati Prakashan, Sultania Rd, Madhya Pradesh 462001
         </span></h6>
         <h6><i className="fa-solid fa-location-dot text-warning"></i> LGS: <span >Near Noor-Us-Sabah Palace, VIP Road, Kohefiza, Bhopal, Madhya Pradesh 462001
         </span></h6>
