a220efb510801a1e302c01bdc2d992250ed1c356
maqbul ansari <maqbul.ansari.dev@gmail.com>
2026-02-26 12:01:10 +0530

    worked

--- src/environment.jsx
+++ src/environment.jsx
@@ -38,8 +38,8 @@ export const img_path = '/';
 // export const TECHNO_BASE_URL = "http://72.61.173.6:8086/techno";
 
 // LG Technohub Production
-// export const AUTH_BASE_URL = "https://api.lgstechnohub.in/auth";
-// export const TECHNO_BASE_URL = "https://api.lgstechnohub.in/techno";
+export const AUTH_BASE_URL = "https://api.lgstechnohub.in/auth";
+export const TECHNO_BASE_URL = "https://api.lgstechnohub.in/auth";
 
 // PythonAnywhere
 //  export const AUTH_BASE_URL = "https://technohub.pythonanywhere.com/auth";
@@ -50,8 +50,8 @@ export const img_path = '/';
 // export const TECHNO_BASE_URL = "https://9gqxjbjg-8000.inc1.devtunnels.ms/techno";
 
 // DevTunnel - Farha
-export const AUTH_BASE_URL = "https://xbzp7968-7000.inc1.devtunnels.ms/auth";
-export const TECHNO_BASE_URL = "https://xbzp7968-7000.inc1.devtunnels.ms/auth";
+// export const AUTH_BASE_URL = "https://xbzp7968-7000.inc1.devtunnels.ms/auth";
+// export const TECHNO_BASE_URL = "https://xbzp7968-7000.inc1.devtunnels.ms/auth";
 
 // DevTunnel - Saba
 // export const AUTH_BASE_URL = "https://958cp4w5-8000.inc1.devtunnels.ms/s";
